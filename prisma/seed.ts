2b17cd378db356eee38b48f93281beee50864f36
Josana-Kla <josana.0205@gmail.com>
2023-01-28 18:46:44 -0300

    fix: prisma seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,23 @@
 import prisma from '../src/database/database.js';
 
 async function main(){
-    data: [
-        {
-            "name": "Mariazinha",           
-            "photo": "https://i.pinimg.com/736x/5e/f4/b7/5ef4b73cf1941ca22620dc87b37d8782.jpg",
-            "email": "ma@gmail.com",      
-            "phone": "2345677345"        
-        }
-    ]
+    await prisma.user.createMany({
+        data: [
+            {
+                "name": "Mariazinha",           
+                "photo": "https://i.pinimg.com/736x/5e/f4/b7/5ef4b73cf1941ca22620dc87b37d8782.jpg",
+                "email": "ma@gmail.com",      
+                "phone": "2345677345"        
+            }
+        ]
+    })
 }
 
 main()
-.then(async () => {
+.then(() => {
     console.error("Success!");
     })
-.catch(async (e) => {
+.catch((e) => {
     console.error(e);
     process.exit(1);
 })
